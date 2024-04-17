26f9ed7b07391e91162a26399492bc89c9f11f02
jeremy <517429+JeremyMelton@users.noreply.github.com>
2024-04-16 23:01:53 -0600

    chore: removed  unused using

--- src/Transmitly/Configuration/Pipeline/Delivery/DeliveryReportConfigurationBuilder.cs
+++ src/Transmitly/Configuration/Pipeline/Delivery/DeliveryReportConfigurationBuilder.cs
@@ -12,8 +12,6 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 
-using Transmitly.ChannelProvider;
-
 namespace Transmitly.Delivery.Configuration
 {
 	public sealed class DeliveryReportConfigurationBuilder
