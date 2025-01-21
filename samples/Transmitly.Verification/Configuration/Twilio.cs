852ae6b8b6ece267390827b98bf0991dd073064d
jeremy <517429+JeremyMelton@users.noreply.github.com>
2025-01-21 13:06:04 -0700

    remove validation sample

--- samples/Transmitly.Verification/Configuration/Twilio.cs
+++
@@ -1,9 +0,0 @@
-namespace VerifyV2Quickstart.Configuration
-{
-    public class Twilio
-    {
-        public string AccountSid { get; set; } = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
-        public string AuthToken { get; set; } = "aXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
-        public string VerificationSid { get; set; } = "VAXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
-    }
-}
